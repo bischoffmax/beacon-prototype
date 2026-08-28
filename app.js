b1895c45161e593d4c7bab462fcf29860f38e0f5
bischoffmax <33343168+bischoffmax@users.noreply.github.com>
2026-08-28 16:46:39 +0200

    Real time distance calculation

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
 const addBeaconButton = document.getElementById("addBeaconButton");
 const locationOutput = document.getElementById("locationOutput");
+const distanceOutput = document.getElementById("distanceOutput");
 const savedBeacon = localStorage.getItem("beacon");
+let updateCount = 0;
 
-if (savedBeacon) {
-    const beacon = JSON.parse(savedBeacon);
-    locationOutput.textContent = `${beacon.name}: ${beacon.latitude}, ${beacon.longitude}`; 
+locationOutput.textContent = "Bitte fügen Sie ein Beacon hinzu."
+
+function toRad(value){
+    return value * Math.PI / 180;
 }
 
-addBeaconButton.addEventListener("click", () => {
-    locationOutput.textContent = "Klick erkannt - Standort wird geladen..."
+function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371000;
 
+    const dLat = toRad(lat2 - lat1);
+    const dLon = toRad(lon2 - lon1);
+
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const distance = R * c;
+
+    return distance;
+}
+
+// Ist bereits ein Beacon vorhanden?
+if (savedBeacon) {
+    const beacon = JSON.parse(savedBeacon);
+
+    navigator.geolocation.watchPosition((position) => {
+        updateCount++;
+        const myLocation = {
+            name: "Handy",
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        }
+        
+        locationOutput.textContent = 
+        `${beacon.name}: ${beacon.latitude}, ${beacon.longitude}`;
+       
+        distanceOutput.textContent = 
+        `Entfernung: ${Math.round(calculateDistance(
+            beacon.latitude, 
+            beacon.longitude,
+            myLocation.latitude,
+            myLocation.longitude))} m | Updates ${updateCount}`;
+    });
+}
+
+// Beacon hinzufuegen
+addBeaconButton.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition((position) => {
         const beacon = {
             name: "Zelt",
@@ -19,6 +63,6 @@ addBeaconButton.addEventListener("click", () => {
 
         localStorage.setItem("beacon", JSON.stringify(beacon));
 
-        locationOutput.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
+        locationOutput.textContent = `${beacon.name} Position: ${beacon.latitude}, ${beacon.longitude}`
     });
 });
